d0ef580ca6fb013b271c567ee0edd9f85f57d82a
YANG-MING HSU <yangminh@andrew.cmu.edu>
2020-03-24 21:15:26 -0400

    To install social_django: pip install social-auth-app-django

--- team23/settings.py
+++ team23/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
+# To install social_django: pip install social-auth-app-django
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
