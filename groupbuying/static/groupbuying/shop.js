a701b0559c0684f95abdc2837fe8ef162d5fe8db
Cheng-chieh, Lin <L.Chengchieh@gmail.com>
2020-03-29 00:19:49 -0400

    Add modal to the edit function.

--- groupbuying/static/groupbuying/shop.js
+++ groupbuying/static/groupbuying/shop.js
@@ -3,4 +3,60 @@ $('#list-menu a').on('click', function (e) {
     $(this).tab('show')
   })
 
-//   alert("Hi")
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the modal context
+var modalContext = $("#myModal #context");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+$("i").click(function(){
+    modalContext.append(getModal())
+    modal.style.display = "block";
+});
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+  modalContext.remove();
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+    modalContext.remove();
+  }
+}
+
+function getModal() {
+    var contextForm = `<form>
+    <div class="form-row">
+      <div class="form-group col-6">
+        <label for="name">Name</label>
+        <input type="text" class="form-control" id="name" placeholder="Name">
+      </div>
+      <div class="form-group col-6">
+        <label for="price">Price</label>
+        <input type="text" class="form-control" id="price" placeholder="Price">
+      </div>
+    </div>
+    <div class="form-group">
+      <label for="description">Description</label>
+      <textarea class="form-control" id="description" rows="3"></textarea>
+    </div>
+    <div class="form-group">
+      <label for="image">Image</label>
+      <input type="file" class="form-control-file" id="image">
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>`
+  return contextForm;
+}
+
