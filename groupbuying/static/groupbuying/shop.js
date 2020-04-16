6bd9c58e5a00b01895c6b8aa228fc30e39b0a68c
Cheng-chieh, Lin <L.Chengchieh@gmail.com>
2020-04-16 01:23:34 -0400

    Add tag search input and filter function.

--- groupbuying/static/groupbuying/shop.js
+++ groupbuying/static/groupbuying/shop.js
@@ -5,6 +5,24 @@ $('#list-tab a').on('click', function (e) {
     $(this).tab('show')
   })
 
+// Tag
+function filterFunction() {
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  div = document.getElementById("myDropdown");
+  a = div.getElementsByTagName("a");
+  for (i = 0; i < a.length; i++) {
+    txtValue = a[i].textContent || a[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = "";
+    } else {
+      a[i].style.display = "none";
+    }
+  }
+}
+
+
 // Get the modal
 $('#dishModal').on('show.bs.modal', function (event) {
   var button = $(event.relatedTarget)
