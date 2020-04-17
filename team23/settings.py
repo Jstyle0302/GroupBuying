3570d850c80d8b4515e545809243bb7e71b5c159
CY <chihy@andrew.cmu.edu>
2020-04-17 14:33:36 -0400

    [Title] Fix bug of email setting

--- team23/settings.py
+++ team23/settings.py
@@ -31,7 +31,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'groupbuyingTeam23@gmail.com'
 EMAIL_HOST_PASSWORD = '1qaz2wsx#E'
-EMAIL_USE_TLS = 'True'
+EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 
 # Application definition
