5dd0308384c3795fd20cb84d9dffffd83dd06bf6
Cheng-chieh, Lin <L.Chengchieh@gmail.com>
2020-04-17 23:14:48 -0400

    Add product add and minus javascript function.

--- groupbuying/static/groupbuying/shop.js
+++ groupbuying/static/groupbuying/shop.js
@@ -1,133 +1,15 @@
+function addProduct(id) {
+  var target = $("#"+id.toString())
+  var currentValue = parseInt(target.val())
+  target.val(currentValue+1)
+}
 
-/* Start for shop.html */
-$('#list-tab a').on('click', function (e) {
-    e.preventDefault()
-    $(this).tab('show')
-  })
-
-// Tag
-function filterFunction() {
-  var input, filter, ul, li, a, i;
-  input = document.getElementById("myInput");
-  filter = input.value.toUpperCase();
-  div = document.getElementById("myDropdown");
-  a = div.getElementsByTagName("a");
-  for (i = 0; i < a.length; i++) {
-    txtValue = a[i].textContent || a[i].innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      a[i].style.display = "";
-    } else {
-      a[i].style.display = "none";
-    }
+function minusProduct(id) {
+  var target = $("#"+id.toString())
+  var currentValue = parseInt(target.val())
+  if (currentValue - 1 > 1) {
+    target.val(currentValue-1)
+  } else {
+    target.val(1)
   }
 }
-
-
-// Get the modal
-$('#dishModal').on('show.bs.modal', function (event) {
-  var button = $(event.relatedTarget)
-  var id = button.data('whatever')
-  var modal = $(this)
-
-  var name = $("#product-name-".concat(id)).text();
-  var price = $("#product-price-".concat(id)).text();
-  var description = $("#product-description-".concat(id)).text();
-  modal.find('#modal-name input').val(name);
-  modal.find('#modal-price input').val(price);
-  modal.find('#modal-description textarea').val(description);
-  modal.find('#modal-id').val(id);
-})
-
-$('#menuModal').on('show.bs.modal', function (event) {
-  var button = $(event.relatedTarget)
-  var name = button.data('whatever')
-  var id = button.attr('id')
-  var modal = $(this)
-  modal.find('#modal-menu-name input').val(name);
-  modal.find('#modal-menu-id').val(id);
-})
-
-$('#vendorModal').on('show.bs.modal', function (event) {
-  var button = $(event.relatedTarget)
-  var name = button.data('whatever')
-  var id = button.attr('id')
-  var modal = $(this)
-  modal.find('#modal-vendor-name input').val(name);
-  modal.find('#modal-vendor-id').val(id);
-})
-
-// google.js
-google.charts.load('current', {'packages':['corechart','bar']});
-google.charts.setOnLoadCallback(drawLineChart);
-
-function drawLineChart() {
-  var data = google.visualization.arrayToDataTable([
-    ['Year', 'Sales', 'Expenses'],
-    ['2004',  1000,      400],
-    ['2005',  1170,      460],
-    ['2006',  660,       1120],
-    ['2007',  1030,      540]
-  ]);
-
-  var options = {
-    curveType: 'function',
-    height: 300,
-    width: 800
-  };
-
-  var chart = new google.visualization.LineChart(document.getElementById('statistics_line'));
-
-  chart.draw(data, options);
-}
-
-
-// google.charts.load('current', {'packages':['bar']});
-google.charts.setOnLoadCallback(drawBarChart);
-
-function drawBarChart() {
-  var data = google.visualization.arrayToDataTable([
-    ['Year', 'Coffee', 'Tea', 'Cake'],
-    ['2014', 1000, 400, 200],
-    ['2015', 1170, 460, 250],
-    ['2016', 660, 1120, 300],
-    ['2017', 1030, 540, 350]
-  ]);
-
-  var options = {
-    bars: 'vertical', // Required for Material Bar Charts.
-    height: 300,
-    width: 800
-  };
-
-  var chart = new google.visualization.ColumnChart(document.getElementById('statistics_bar'));
-
-  chart.draw(data, google.charts.Bar.convertOptions(options));
-}
-
-
-// google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(drawPieChart);
-
-function drawPieChart() {
-
-  var data = google.visualization.arrayToDataTable([
-    ['Customers', 'Number'],
-    ['Under 20',     11],
-    ['21 to 30',      2],
-    ['31 to 40',  2],
-    ['41 to 50', 2],
-    ['Above 51',    7]
-  ]);
-
-  var options = {
-    legend: {position:'labeled',alignment:'end'},
-    height: 500,
-    width: 800
-  };
-
-  var chart = new google.visualization.PieChart(document.getElementById('statistics_pie'));
-
-  chart.draw(data, options);
-}
-
-/* End for shop.html */
