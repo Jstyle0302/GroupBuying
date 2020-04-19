99fa6a4e051bdeabcf14883d0a0a7f06e6d628cf
CY <chihy@andrew.cmu.edu>
2020-04-19 12:50:39 -0400

    [Title] If already logged in, registration redirect to home

--- groupbuying/views.py
+++ groupbuying/views.py
@@ -1453,6 +1453,8 @@ def logout_action(request):
 def register_action(request):
     context = {}
 
+    if request.user.is_authenticated:
+        return redirect(reverse('home'))
     # Just display the registration form if this is a GET request.
     if request.method == 'GET':
         context['form'] = RegistrationForm()
